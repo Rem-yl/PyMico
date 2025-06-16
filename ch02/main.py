f9e386d6f5cda99f95cb4dbc510e565264364227
ley <ley@opera.com>
2025-06-16 16:01:47 +0800

    Add HTTP middleware to log request details and track response time

--- ch02/main.py
+++ ch02/main.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from typing import Awaitable, Callable
+
 import feedback
 import login
 import places
@@ -5,7 +8,7 @@ import tourist
 from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
 from fastapi.requests import Request
-from fastapi.responses import JSONResponse, PlainTextResponse
+from fastapi.responses import JSONResponse, PlainTextResponse, Response
 from handler import PostFeedbackException, PostRatingException
 from starlette.exceptions import HTTPException as GlobalStarletteHTTPException
 
@@ -44,3 +47,23 @@ def validation_exception_hander(
     req: Request, ex: RequestValidationError
 ) -> PlainTextResponse:
     return PlainTextResponse(f"Error message: {str(ex)}", status_code=400)
+
+
+@app.middleware("http")
+async def log_transaction_filter(
+    req: Request, call_next: Callable[[Request], Awaitable[Response]]
+) -> Response:
+    start_time = datetime.now()
+    method_name = req.method
+    qp_map = req.query_params
+    pp_map = req.path_params
+
+    with open("request.log", mode="a", encoding="utf-8") as f:
+        content = f"method: {method_name}, query param: {qp_map}, path params: {pp_map} received at {datetime.now()}\n"
+        f.write(content)
+
+    response = await call_next(req)
+    process_time = datetime.now() - start_time
+    response.headers["X-Time-Elapsed"] = str(process_time)
+
+    return response
